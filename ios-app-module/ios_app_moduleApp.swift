534a1ec57c9d4b3f7d5bc495ace3ff3131a1a91e
hieptv11 <hieptv11@viettel.com.vn>
2024-04-12 09:12:39 +0700

    feat: init project

---
+++ ios-app-module/ios_app_moduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_moduleApp.swift
+//  ios-app-module
+//
+//  Created by Tran Van Hiep on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_moduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
